56ff79243397e52d0d316f1dfe59b0d057887058
Pasindu P Konghawaththa <pasindudixon25@gmail.com>
2022-02-25 20:29:35 +0530

    rewamp typeorm

--- quiz-mind-server/src/app.module.ts
+++ quiz-mind-server/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { typeOrmConfig } from './config/typeorm.config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
-  controllers: [],
-  providers: [],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
