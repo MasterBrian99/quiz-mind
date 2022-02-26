f46a1bb21544f06c67e1cdb5a729279ade27ffff
Pasindu P Konghawaththa <pasindudixon25@gmail.com>
2022-02-26 13:23:10 +0530

    add signin page

--- quiz-mind-client/src/screens/LoginScreen/LoginScreen.tsx
+++ quiz-mind-client/src/screens/LoginScreen/LoginScreen.tsx
@@ -1,14 +1,30 @@
-import React from "react";
-import styles from "./LoginScreen.module.scss";
-
 const LoginScreen = () => {
   return (
-    <div className="flex">
-      <div className="flex-1">
-        <h1>Hello</h1>
-      </div>
-      <div className="flex-1">
-        <h1>Hello</h1>
+    <div
+      className={`login-signup-screen flex flex-col justify-center items-center`}
+    >
+      <div className="bg-slate-50 p-12 flex flex-col justify-center items-center text-deepslate-400 rounded-xl">
+        {/* <img src="../../assets/images/darklogo.png" alt="" className="w-24" /> */}
+        <h4 className=" font-bold text-3xl">Sign in</h4>
+        <h5 className="text-2xl">Sign into your account for full access</h5>
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="username"
+          className="w-full border-2 border-blue-400 rounded-lg py-2 px-2 outline-none text-xl font-semibold mt-2 mb-2"
+        />
+        <input
+          type="password"
+          name=""
+          id=""
+          className="w-full border-2 border-blue-400 rounded-lg py-2 px-2 outline-none text-xl font-semibold mt-2 mb-4"
+          placeholder="password"
+        />
+        <button className="bg-blue-400 w-full py-3 rounded-lg text-extrawhite-300  font-bold ">
+          Sign in
+        </button>
+        <p className="mt-2">Don't have an account yet? Sign up</p>
       </div>
     </div>
   );
